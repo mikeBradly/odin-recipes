63bf978158b483f4653d0d6663c07096ce699a10
mikeBradly <139893265+mikeBradly@users.noreply.github.com>
2023-09-20 12:53:30 -0400

    Feat text description with corresponding picture

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,17 +1,21 @@
 const images = ["./images/gc-chicken.jpeg", "./images/hdspaghetti.jpeg", "./images/pork.jpeg"]; // Add image URLs here
 const paths =["./recipes/garlicChicken.html", "./recipes/spaghetti.html", "./recipes/tenderloin.html"]; // Add relative paths here
 const alts =["Garlic-Caper Chicken", "Quick Hot Dog Spaghetti", "Sheet Pan Pork with Roasted Vegetables"];
+const descriptions = ["Delicious Garlic-Caper Chicken Recipe", "Quick and Easy Hot Dog Spaghetti Recipe", "Sheet Pan Pork with Roasted Vegetables Recipe"];
+
 let currentIndex = 0;
 
 const prevButton = document.getElementById("prev-btn");
 const nextButton = document.getElementById("next-btn");
 const image = document.getElementById("image");
 const imageLink = document.getElementById("image-link");
+const imageDescription = document.getElementById("image-description");
 
 function showImage(index) {
     image.src = images[index];
     imageLink.href = paths[index]; // Set the anchor's href to the corresponding relative path
     image.alt = alts[index];
+    imageDescription.textContent = descriptions[index];
 }
 
 prevButton.addEventListener("click", () => {
